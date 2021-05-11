3b74d85285ece7e36989e2dd75dbed842ebfbd55
Sam0bt <sami.babat@gmail.com>
2021-05-11 23:57:07 +0300

    rvgl fix

--- rvgl/actions.py
+++ rvgl/actions.py
@@ -5,6 +5,5 @@ from pisi.actionsapi import get, pisitools, shelltools
 
 def install():
     pisitools.insinto("/usr/share/rvgl", "./*")
-    pisitools.dosym("/usr/share/rvgl/rvgl.64", "/usr/bin/rvgl")
     pisitools.dosym("/usr/lib/libfluidsynth.so", "/usr/lib/libfluidsynth.so.1")
 
